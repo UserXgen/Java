cb36a7fcc76447bf465f612266c976c94b002ccb
UserXgen <aleksey.pavlov2013@mail.ru>
2022-04-09 14:13:28 +0300

    Lab5

---
+++ Lab5/Lab5/src/my/numberaddition/MyThread.java
@@ -0,0 +1,58 @@
+
+package my.numberaddition;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+public class MyThread extends Thread {
+    
+    private double area;
+    private double lowLim;
+    private double upLim;
+    private double stepInteg;
+    private int iteration; 
+    private int endIteration;
+    private IntegrSearch.Function function;
+    
+    public boolean ret = false;
+    
+    MyThread(int vIteration, int vEndIteration, double vLowLim, double vUpLim, double vStepInteg, IntegrSearch.Function func){
+       
+        iteration = vIteration;
+        endIteration = vEndIteration;      
+        lowLim = vLowLim;
+        upLim = vUpLim;
+        stepInteg = vStepInteg;
+        function = func;
+       
+    }
+    
+    
+    public void run(){
+    
+        integralPartTrap();
+        
+        
+    }
+    
+    public double GetArea(){
+    
+        return area;
+        
+    }
+    
+
+    public void integralPartTrap() {
+        area = 0;
+ 
+        for (int i = iteration; i < endIteration; i++) {
+            
+           // System.out.println(iteration + " " + endIteration);
+            
+            area += stepInteg * (0.5 * (function.func(lowLim + i * stepInteg)+function.func(lowLim+(i+1)* stepInteg)));
+        }
+    
+    }
+    
+}
